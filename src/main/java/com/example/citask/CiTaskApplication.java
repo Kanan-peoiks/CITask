d3e9da893c76cedc18c2d0b1dec0629ac4c4e8cd
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-31 19:33:58 +0400

    first commit

---
+++ src/main/java/com/example/citask/CiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.citask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiTaskApplication.class, args);
+    }
+
+}
