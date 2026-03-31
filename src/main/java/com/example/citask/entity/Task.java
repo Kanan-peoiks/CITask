be4690e006a6c5e1ee38cc2af3d5530f57def314
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-31 19:48:23 +0400

    feat: add entity package

---
+++ src/main/java/com/example/citask/entity/Task.java
@@ -0,0 +1,30 @@
+package com.example.citask.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String completed;
+
+    private LocalDateTime createdAt;
+
+}
